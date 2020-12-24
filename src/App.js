185025fa508f8e286323b74061561f9f16dce226
Chirag <agarwalchirag112@gmail.com>
2020-12-24 12:47:33 +0530

    add comment section in git

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import './App.css';
 import Home from "./components/Home/Home";
 import { BrowserRouter, Route } from "react-router-dom";
-import Comment from "./components/CommentSection/Comment"
-import Header from "./components/header";
+import Comment from "./components/CommentSection/Comment";
 import userImg from "./images/demoUserImg.jpg";
 import Story from "./components/StoryHeader/Userstories";
 
@@ -19,63 +18,62 @@ function App() {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username1"
     },
     {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username2"
     },
     {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username3"
     },
     {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username4"
     },
     {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username5"
     },
     {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username6"
     },
     {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username7"
     },
     {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username8"
     },
     {
       posturl: userImg,
       postCaption: "#carying",
       avatarUrl: userImg,
-      userName: "username"
+      userName: "username9"
     }])
   return (<BrowserRouter>
     <div className="app">
     
     <Route exact path="/"> <Home post={post} /></Route>
     <Route exact path="/comment"><Comment data={post}/></Route>
-    <Route exact path="/story"><Story data={post}/></Route>
-     
+    <Route exact path="/story"><Story data={post}/></Route>    
     </div>
   </BrowserRouter>
   );
--- src/components/CommentSection/Comment.js
+++ src/components/CommentSection/Comment.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import Header from "../header"
+import React, { useState } from 'react';
+import Header from "../header";
 import commentStyle from "../../styles/Comment.module.css";
 import arrImg from "../../images/arrcommentimg.png";
 import userimg from "../../images/demoUserImg.jpg";
 
 export default function Comment(props) {
+    const [chatperson, setChatperson] = useState();
+    const [chatData, setChatData] = useState([]);
+    const [message, setMessage] = useState([]);
+
+
+    const chatDataHandler = (e) => {
+
+        setChatData([e.target.value])
+
+    }
+
+    const messageHandler = () => {
+        setMessage([...message, chatData]);
+    }
+
+
+    const chatHandler = (e, val) => {
+        console.log(val.userName);
+
+        setChatperson({
+            userName: val.userName
+        });
+        console.log(chatperson);
+    }
     return (<>
-    <Header />
+        <Header />
         <div className={commentStyle.ext_div}>
             <div className={commentStyle.center_div}>
                 <div className={commentStyle.leftside_portion}>
@@ -15,23 +39,50 @@ export default function Comment(props) {
                     </div>
                     <div className={commentStyle.userNames}>
                         {
-                            props.data.map((val) => {
-                                return(<>
+                            props.data.map((val, i) => {
+                                return (<div key={i} onClick={(e) => {
+                                    chatHandler(e, val)
+                                }}>
                                     <img className={commentStyle.userimg} alt="..." src={userimg}></img>
-                                    <span className={commentStyle.username}>{val. userName}</span>
-                                </>)
+                                    <span className={commentStyle.username}>{val.userName}</span>
+                                </div>)
                             })
                         }
                     </div>
                 </div>
+
+
                 <div className={commentStyle.rightside_portion}>
-                    <img src={arrImg} alt="..." className={commentStyle.arrimg} />
-                    <p className={commentStyle.text}>Your Messages</p>
-                    <p className={commentStyle.text2}>Send private photos and messages to a friend or group.</p>
+                    {
+
+                        (!chatperson) ? <div>
+                            <img src={arrImg} alt="..." className={commentStyle.arrimg} />
+                            <p className={commentStyle.text}>Your Messages</p>
+                            <p className={commentStyle.text2}>Send private photos and messages to a friend or group.</p>
+                        </div>
+                            : <div>
+                                <div className={commentStyle.chatHeader}>
+                                    <img className={commentStyle.chatUserImg} alt="..." src={userimg}></img>
+                                    <p className={commentStyle.chatUserName}> {chatperson.userName}</p>
+                                    <div className={commentStyle.messagePart}>
+                                        {
+                                            message.map((val, i) => {
+                                                return <p className={commentStyle.message} key={i}>{val}
+                                                         
+                                                </p>
+                                            })
+                                        }
+                                    </div>
+                                    <input className={commentStyle.chatInput} type="text" value={chatData} placeholder="Message" onChange={chatDataHandler}></input>
+                                    <button className={commentStyle.sendButton} onClick={messageHandler}><b>send</b></button>
+                                </div>
+
+                            </div>
+                    }
                 </div>
             </div>
 
         </div>
-        </>
+    </>
     )
 }

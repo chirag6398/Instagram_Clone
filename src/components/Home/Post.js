ecde6af7c76af5cd9a373dee4e68befbda1ebeed
Chirag <agarwalchirag112@gmail.com>
2020-12-25 15:04:08 +0530

    adding more facility like button ..

--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,18 +1,35 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import postStyle from "../../styles/Post.module.css";
 import Avatar from "@material-ui/core/Avatar";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import heartIcon from "../../images/hearticon.png";
+import commentIcon from "../../images/comment.png";
+import arrIcon from "../../images/arrow.png";
+import redHrt from "../../images/redHrt.png"
 
 export default function Post(props) {
-   const [comments,setComments]=useState({comment:""});
-   const [postedComments,setPostedComment]=useState({postedComment:""})
-    const commentHandler=(e)=>{
-      setComments({comment:e.target.value})
+    const [comments, setComments] = useState({ comment: "" });
+    const [postedComments, setPostedComment] = useState({ postedComment: "" });
+    const [showComment, setShowComment] = useState(false);
+    const [count, setCount] = useState(1);
+    const [count2, setCount2] = useState(1);
+
+    const hrtHandler2 = () => {
+        setCount2(count2 + 1);
     }
-    const commentPostHandler=()=>{
-        setPostedComment({postedComment:comments.comment})
-        setComments({comment:""})
+    const hrtHandler = () => {
+        setCount(count + 1);
+    }
+
+    const directedToComment = () => {
+        setShowComment(true);
+    }
+    const commentHandler = (e) => {
+        setComments({ comment: e.target.value })
+    }
+    const commentPostHandler = () => {
+        setPostedComment({ postedComment: comments.comment })
+        setComments({ comment: "" })
     }
     return (
         <div className={postStyle.post_container} >
@@ -25,19 +42,32 @@ export default function Post(props) {
 
 
             <img src={props.userimg} className={postStyle.user_img} alt="..." />
-
-            <p className={postStyle.post_caption}><strong>@username</strong>   caption #crazy#innovative {props.caption}</p>
-            
-           {(postedComments.postedComment)? <p className={postStyle.post_caption}><strong>@username</strong> {postedComments.postedComment} 
-           <img src={heartIcon} alt="..." className={postStyle.hrtIcon}/> </p>:null}
-            <div className="input-group">
-                <div className="custom-file">
-                    <input type="text" className="form-control" placeholder="Add a comment.." value={comments.comment} name="comment" onChange={commentHandler} />
+            <div className={postStyle.belowPost}>
+                <div className={postStyle.postIcons}>
+                    <img className={postStyle.hrtPIcon} src={(count2 % 2 === 0) ? redHrt : heartIcon} onClick={hrtHandler2} alt="" />
+                    <img className={postStyle.cmntIcon} src={commentIcon} alt="" onClick={directedToComment} />
+                    <img className={postStyle.arrIcon} src={arrIcon} alt="" />
                 </div>
-                <div className="input-group-append">
-                    <span className={postStyle.post} onClick={commentPostHandler} >post</span>
-                </div>
+                <p>Liked by kushagra8198 and 90 others</p>
+                <p className={postStyle.post_caption}><strong>@username</strong>   caption #crazy#innovative {props.caption}</p>
+                <p style={{ opacity: ".5" }}>19 HOURS AGO</p>
             </div>
+
+
+            {(postedComments.postedComment) ? <p style={{ paddingLeft: "7px" }}><strong>@username</strong> {postedComments.postedComment}
+                <img src={(count % 2 === 0) ? redHrt : heartIcon} alt="..." className={postStyle.hrtIcon} onClick={hrtHandler} /> </p> : null}
+
+            {
+                (showComment) ? <div className="input-group">
+                    <div className="custom-file">
+                        <input type="text" className="form-control" placeholder="Add a comment.." value={comments.comment} name="comment" onChange={commentHandler} />
+                    </div>
+                    <div className="input-group-append">
+                        <span className={postStyle.post} onClick={commentPostHandler} >post</span>
+                    </div>
+                </div> : null
+
+            }
 
         </div>
     )
